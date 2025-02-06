907d63d303ec10302c069d149d3d3547e00b0ac9
Muserref Ebru Saygili <ebru_zltn@yahoo.com>
2025-02-06 22:28:24 +0300

    Update MaxSliceSum.java
    https://app.codility.com/demo/results/trainingR2V9QY-J9J/

--- MaxSliceSum.java
+++ MaxSliceSum.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int[] A) {
         // write your code in Java SE 8
-        if(A.length==1) return A[0];
-        int maxSlice=Integer.MIN_VALUE;
-        int candidate;
-        for(int i=0;i<A.length;i++){
-            candidate=A[i];
-            if(maxSlice<candidate) maxSlice=candidate;
-            for(int j=i+1;j<A.length;j++){
-                candidate+=A[j];
-                if(maxSlice<candidate) maxSlice=candidate;
-            }
+        int maxSlice=A[0];
+        int candidateMax=A[0];
+        for(int i=1;i<A.length;i++){
+            candidateMax=Math.max(A[i],candidateMax+A[i]);
+            maxSlice=Math.max(maxSlice, candidateMax);
         }
         return maxSlice;
         
